49e1c1ed643f258b2e4fae9ed2ae8a25daec180b
Olegborodko <220v2@mail.ru>
2017-06-12 22:58:56 +0300

    test fix

--- spec/controllers/works_controller_spec.rb
+++ spec/controllers/works_controller_spec.rb
@@ -7,6 +7,20 @@ DatabaseCleaner.clean
 
 describe WorksController do
 
+  before do
+    @api = create(:api)
+    @location = create(:location)
+    @weather1 = create(:work, location_id: @location.id, api_id: @api.id)
+
+    @api2 = create(:api, category: 2)
+    @weather2 = create(:work, location_id: @location.id, api_id: @api2.id)
+
+    @user = controller.send(:current_user)
+
+    @user.works << @weather1
+    @user.works << @weather2
+  end
+
   it "create (city blank)" do
     post :create, params: {form_region: {city: nil, country_key: 'UA'}}, format: :js
     expect(assigns(:error)).to eq('please enter city')
@@ -24,48 +38,39 @@ describe WorksController do
   end
 
   it "create (weather exists)" do
-    api = create(:api, time: Time.now - 2.day)
-    location = create(:location)
-    weather1 = create(:work, location_id: location.id, api_id: api.id)
-
-    api2 = create(:api, category: 2, time: Time.now - 1.month)
-    weather2 = create(:work, location_id: location.id, api_id: api2.id)
-
     expect(Work.all.size).to eq 2
 
     post :create, params: {form_region: {city: 'Odessa', country_key: 'UA'}}, format: :js
 
     expect(Work.all.size).to eq 2
 
-    # expect(api.time.day).to eq Time.now.day
-    # expect(api2.time.month).to eq Time.now.month
+    api = Api.first
+    api2 = Api.last
+
+    expect(api.time.day).to eq Time.now.day
+    expect(api2.time.month).to eq Time.now.month
   end
-  #
-  # it "create (weather new)" do
-  #   weather = create(:work)
-  #   post :create, params: {form_region: {city: 'Kiev', country_key: 'UA'}}, format: :js
-  #
-  #   expect(Work.all.size).to eq 2
-  #   expect(response).to render_template(:create)
-  # end
-  #
-  # it "destroy ok" do
-  #   weather = create(:work)
-  #   user = controller.send(:current_user)
-  #   user.works << weather
-  #
-  #   delete :destroy, params: { id: weather.id }, format: :js
-  #   expect(user.works.size).to eq 0
-  #   expect(response).to render_template(:create)
-  # end
-  #
-  # it "destroy not deleted" do
-  #   weather = create(:work)
-  #   user = controller.send(:current_user)
-  #   user.works << weather
-  #
-  #   delete :destroy, params: { id: 100 }, format: :js
-  #   expect(user.works.size).to eq 1
-  # end
+
+  it "create (weather new)" do
+    post :create, params: {form_region: {city: 'Kiev', country_key: 'UA'}}, format: :js
+
+    expect(Work.all.size).to eq 4
+    expect(Location.all.size).to eq 2
+    expect(User.all.size).to eq 1
+    expect(Api.all.size).to eq 4
+    expect(response).to render_template(:create)
+  end
+
+  it "destroy ok" do
+    delete :destroy, params: { id: @location.id }, format: :js
+    expect(@user.works.size).to eq 0
+    expect(response).to render_template(:create)
+  end
+
+  it "destroy not deleted" do
+    delete :destroy, params: { id: 0 }, format: :js
+    expect(@user.works.size).to eq 2
+    expect(response).to render_template(:create)
+  end
 
 end
